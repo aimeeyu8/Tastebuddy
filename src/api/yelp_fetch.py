4957a58cf12b91b7930aeb556a744fe0b40ef205
Aimee Yu <aty2013@nyu.edu>
2025-11-12 14:54:09 -0500

    yelp set up

---
+++ src/api/yelp_fetch.py
@@ -0,0 +1,19 @@
+from dotenv import load_dotenv
+import os
+import requests
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parent.parent / ".env"
+load_dotenv(dotenv_path=env_path)
+serpapi_key = os.getenv("SERPAPI_API_KEY")
+params = {
+    "engine": "yelp",     
+    "q": "restaurants",
+    "api_key": serpapi_key,
+    "find_loc": "New York, NY, USA"
+}
+
+search = requests.get("https://serpapi.com/search", params=params)
+response = search.json()
+print(response)
+
